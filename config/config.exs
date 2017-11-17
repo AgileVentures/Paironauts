e760eafdc63aaa818d05aee72a8c85d7e3570575
SherSpock <wrtimberlake@gmail.com>
2017-11-17 09:10:21 -0500

    Add hound

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,11 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# Configure hound with phantomjs for automated browser testing  
+config :hound, driver: "phantomjs"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+
--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Paironauts.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:paironauts_otp, path: "paironauts_otp"}
+      {:paironauts_otp, path: "paironauts_otp"},
+      {:hound, "~> 1.0"}
     ]
   end
 
