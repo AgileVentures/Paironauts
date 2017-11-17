28ba1a422f757181150bb945723e56e8b57fc96a
SherSpock <wrtimberlake@gmail.com>
2017-11-17 10:26:43 -0500

    Revert "Add hound"
    This reverts commit e760eafdc63aaa818d05aee72a8c85d7e3570575.

--- mix.exs
+++ mix.exs
@@ -41,8 +41,7 @@ defmodule Paironauts.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:paironauts_otp, path: "paironauts_otp"},
-      {:hound, "~> 1.0"}
+      {:paironauts_otp, path: "paironauts_otp"}
     ]
   end
 
