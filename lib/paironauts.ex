29ec06762f1bd0d68547857e6fbe0650962b78a3
Sam Joseph <tansaku@gmail.com>
2017-11-17 11:40:52 +0000

    trying to set up presence to count websocket connects

--- lib/paironauts.ex
+++ lib/paironauts.ex
@@ -6,4 +6,7 @@ defmodule Paironauts do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  children = [
+    supervisor(Paironauts.LobbyPresence, []),
+  ]
 end
--- lib/paironauts_web/channels/room_channel.ex
+++ lib/paironauts_web/channels/room_channel.ex
@@ -2,7 +2,7 @@ defmodule PaironautsWeb.RoomChannel do
   @moduledoc """
   The interface between the browser and the Paironaut sever.
   """
-
+  alias Paironauts.LobbyPresence
   use Phoenix.Channel
 
   def join("room:pairing", _message, socket) do
@@ -10,13 +10,30 @@ defmodule PaironautsWeb.RoomChannel do
     # https://hexdocs.pm/phoenix/Phoenix.Presence.html
     # when we have two, we could send js to redirect both to room with
     # specific id ... maybe using https://developer.mozilla.org/en-US/docs/Web/API/Location/replace
+   
+    # how many clients listening on websocket
+    # if >2 then broadcast the pairing redirect
+
+
+    send(self(), :after_join)
+    # {:ok, assign(socket, :user_id, ...)}
     {:ok, socket}
   end
 
-  def handle_in(name, %{"body" => content}, socket) do
-    broadcast!(socket, "live_response", %{url: "/pairing"})
+  def handle_info(:after_join, socket) do
+    push socket, "presence_state", Presence.list(socket)
+    {:ok, _} = Presence.track(socket, socket.assigns.user_id, %{
+      online_at: inspect(System.system_time(:seconds))
+    })
     {:noreply, socket}
   end
+
+  # def handle_in(name, %{"pathname" => pathname}, socket) do
+  #   if name == "start_pairing" and pathname == "/" do
+  #     broadcast!(socket, "live_response", %{url: "/pairing"})
+  #   end
+  #   {:noreply, socket}
+  # end
 
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
