121cf5d807f8e5db16b6ad65466e8a09e84a6388
SherSpock <wrtimberlake@gmail.com>
2017-11-17 12:12:35 -0500

    Working on a more appropriate acceptance test
    In doing so, it seems we may need to be handling some errors for Jitsi.
    Rolling back to something a bit more like the working behavior we had
    this morning

--- lib/paironauts_web/channels/room_channel.ex
+++ lib/paironauts_web/channels/room_channel.ex
@@ -20,20 +20,20 @@ defmodule PaironautsWeb.RoomChannel do
     {:ok, socket}
   end
 
-  def handle_info(:after_join, socket) do
-    push socket, "presence_state", LobbyPresence.list(socket)
-    {:ok, _} = LobbyPresence.track(socket, socket.assigns.user_id, %{
-      online_at: inspect(System.system_time(:seconds))
-    })
-    {:noreply, socket}
-  end
-
-  # def handle_in(name, %{"pathname" => pathname}, socket) do
-  #   if name == "start_pairing" and pathname == "/" do
-  #     broadcast!(socket, "live_response", %{url: "/pairing"})
-  #   end
+  # def handle_info(:after_join, socket) do
+  #   push socket, "presence_state", LobbyPresence.list(socket)
+  #   {:ok, _} = LobbyPresence.track(socket, socket.assigns.user_id, %{
+  #     online_at: inspect(System.system_time(:seconds))
+  #   })
   #   {:noreply, socket}
   # end
+
+  def handle_in(name, %{"pathname" => pathname}, socket) do
+    if name == "start_pairing" and pathname == "/" do
+      broadcast!(socket, "live_response", %{url: "/pairing"})
+    end
+    {:noreply, socket}
+  end
 
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
--- test/browser/acceptance_test.exs
+++ test/browser/acceptance_test.exs
@@ -5,9 +5,7 @@ defmodule Paironauts.AcceptanceTest do
   
     test "users have names", %{session: session} do
       session
-      |> visit("/users")
-      |> find(css(".user", count: 3))
-      |> List.first()
+      |> visit("/")
       |> assert_has(css(".user-name", text: "Chris"))
     end
   end
