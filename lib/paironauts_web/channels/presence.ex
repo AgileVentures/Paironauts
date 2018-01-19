70ae5fbc260b079cccf71296432ec2163f09b171
Sam Joseph <tansaku@gmail.com>
2018-01-19 11:10:08 +0000

    adjust naming

--- lib/paironauts_web/channels/presence.ex
+++ lib/paironauts_web/channels/presence.ex
@@ -1,4 +1,4 @@
-defmodule Paironauts.Presence do  
+defmodule PaironautsWeb.Presence do  
   use Phoenix.Presence, otp_app: :paironauts,
                         pubsub_server: Paironauts.PubSub
 end 
