5cf2cd436e0c211f08e80f3820391d4145ea9919
Ben Blanchard <benjamin.blanchard@gmail.com>
2018-03-06 11:01:24 -0500

    Looking for places to put javascript jitsi.

--- assets/js/pairing.js
+++ assets/js/pairing.js
@@ -2,13 +2,14 @@
 // maybe overkill, but I want to think about how we might inject these instead
 // as long as we were to do it without obfuscating stuff or making it super unwieldy
 
-if(typeof JitsiMeetExternalAPI != "undefined") {
-  // var domain = "meet.jit.si";
-  // var options = {
-  //     roomName: document.title,
-  //     width: 700,
-  //     height: 700,
-  //     parentNode: document.querySelector('#meet')
-  // }
-  // var api = new JitsiMeetExternalAPI(domain, options);
+
+if (typeof JitsiMeetExternalAPI != "undefined") {
+  var domain = "meet.jit.si";
+  var options = {
+      roomName: document.title,
+      width: 700,
+      height: 700,
+      parentNode: document.querySelector('#pairing_session')
+  }
+  var api = new JitsiMeetExternalAPI(domain, options);
 }
