1427e4dc42a7eeefb3ff130f2581b6f4491f5687
SherSpock <wrtimberlake@gmail.com>
2018-05-11 16:09:58 -0400

    Config Guardian and Ueberauth

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,32 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :ueberauth, Ueberauth,
+  providers: [
+    github:   {Ueberauth.Strategy.Github, [uid_field: "login"]},
+    identity: {Ueberauth.Strategy.Identity, [callback_methods: ["POST"]]}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id:     System.get_env("GITHUB_CLIENT_ID"),
+  client_secret: System.get_env("GITHUB_CLIENT_SECRET")
+
+config :guardian, Guardian,
+  issuer: "Paironauts.#{Mix.env}",
+  ttl: {30, :days},
+  verify_issuer: true,
+  serializer: PaironautsWeb.GuardianSerializer,
+  secret_key: to_string(Mix.env),
+  hooks: GuardianDb,
+  permissions: %{
+    default: [
+      :read_profile,
+      :write_profile,
+      :read_token,
+      :revoke_token,
+    ],
+  }
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
