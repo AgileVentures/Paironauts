30d2202b1b8c227b8909996ea15da237287c3171
Praveen Dhawan <praveen@sdtechdev.com>
2018-05-21 19:07:33 +0530

    Added support for hound
    Use Hound instead of wallaby for acceptance tests
    due to inconsistency in tests behaviour on different machines

--- test/browser/acceptance_test.exs
+++ test/browser/acceptance_test.exs
@@ -1,8 +1,6 @@
 defmodule Paironauts.AcceptanceTest do
-    use PaironautsWeb.BrowserCase, async: true
-
-    import Wallaby.Query
-    alias Wallaby.Query
+    use ExUnit.Case
+    use Hound.Helpers
 
     # Right now it works so I don't really want to break it...
     # Just put that functionality somewhere else.
@@ -17,39 +15,38 @@ defmodule Paironauts.AcceptanceTest do
     # Then I should see "preparing your pair room"
     # And the other user should see the room load
     # And I should see the room should load
+    hound_session()
 
-    test "when a user chooses 'mob' from the homepage, they are added to a single shared Jitsi", %{session: session} do
+    test "when a user chooses 'mob' from the homepage, they are added to a single shared Jitsi", _meta do
       # This is barebones.
       # We should check that users are being added to the Jitsi
-      session
-      |> visit("/")
-      |> click(css("#mob"))
-      |> assert_has(css("#meet"))
+      navigate_to("/")
+      element = find_element(:id, "mob")
+      click(element)
+
+      assert(find_element(:id, "meet"))
     end
 
     # third user should not be added to the same jitsi
-    test "when two users choose 'pair' from the homepage, they are added to a Jitsi", %{session: session1} do
-      session1
-      |> visit("/") 
-      |> click(css("#pair"))
-      |> has_text?("Waiting for pair partner")
-      |> assert
+    test "when two users choose 'pair' from the homepage, they are added to a Jitsi", _meta do
+      in_browser_session"session1", fn ->
+        navigate_to("/")
+        element = find_element(:id, "pair")
+        click(element)
+        assert(visible_text({:css, "#wait"}), "Waiting for pair partner")
+      end
 
-      # will see "waiting for pair"
-      # and jitsi visible
-      # |> assert_has(css("#meet"))  # checking for the meet div
+      in_browser_session"session2", fn ->
+        navigate_to("/")
+        element = find_element(:id, "pair")
+        click(element)
 
-      {:ok, session2} = Wallaby.start_session
-      session2
-      |> visit("/")
-      |> click(css("#pair"))
-      |> find(css("#pairing_session"))
-      |> has_text?("Waiting for pair partner")
-      |> refute
+        element = find_element(:id, "wait")
+        assert(visible_text(element), "Pairing session")
+      end
 
-      session1
-      |> has_text?("Pairing session")
-      |> assert
+      change_session_to("session1")
+      assert(visible_text({:css, "#wait"}), "Pairing session")
 
       # check redirected to /pairing-2345678 /pairing/2345678
       # check
@@ -61,39 +58,41 @@ defmodule Paironauts.AcceptanceTest do
       # |> assert
 
     end
-
+    #
     # ensure only two users arrive in pairing session (and both of them were waiting to pair)
     # 1. don't drag users off other pages
     # 2. don't drag users out of existing pairing sessions
+    #
+    test "user on home page who hasn't chosen to pair is not pulled into pairing session", _meta do
+      in_browser_session"first_user_wanting_to_pair", fn ->
+        navigate_to("/")
+        element = find_element(:id, "pair")
+        click(element)
 
-    test "user on home page who hasn't chosen to pair is not pulled into pairing session", %{session: first_user_wanting_to_pair} do
-      first_user_wanting_to_pair
-      |> visit("/")
-      |> click(css("#pair"))
-      |> assert_has(css("#wait", text: "Waiting for pair partner..."))
-
-      #require IEx ; IEx.pry
-      # |> assert_has(Query.text("Waiting for pair partner..."))
+        assert(visible_text({:css, "#wait"}), "Waiting for pair partner")
+      end
 
       # user on home page (also need to check for user in pairing session, and third user waiting)
-      {:ok, user_not_wanting_to_pair} = Wallaby.start_session
-      user_not_wanting_to_pair
-      |> visit("/")
-      |> refute_has(css("#wait", text: "Waiting for pair partner..."))
+      in_browser_session"user_not_wanting_to_pair", fn ->
+        navigate_to("/")
 
-      {:ok, second_user_wanting_to_pair} = Wallaby.start_session
-      second_user_wanting_to_pair
-      |> visit("/")
-      |> click(css("#pair"))
-      |> refute_has(css("#wait", text: "Waiting for pair partner..."))
-      |> assert_has(css("#pairing_session", text: "Pairing session"))
+        assert(find_element(:id, "pair"))
+      end
 
-      first_user_wanting_to_pair
-      |> assert_has(css("#pairing_session", text: "Pairing session"))
+      in_browser_session"second_user_wanting_to_pair", fn ->
+        navigate_to("/")
+        element = find_element(:id, "pair")
+        click(element)
 
-      user_not_wanting_to_pair
-      |> refute_has(css("#pairing_session", text: "Pairing session"))
+        element = find_element(:id, "wait")
+        assert(visible_text(element), "Pairing session")
+      end
 
+      change_session_to("first_user_wanting_to_pair")
+      assert(visible_text({:css, "#wait"}), "Pairing session")
+
+      change_session_to("user_not_wanting_to_pair")
+      assert(find_element(:id, "pair"))
     end
     # ensure that a third user somewhere on the site is not dragged into the pairing room
     # test '...'
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,6 @@
+Application.ensure_all_started(:hound)
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Paironauts.Repo, :manual)
-{:ok, _} = Application.ensure_all_started(:wallaby)
+# {:ok, _} = Application.ensure_all_started(:wallaby)
 {:ok, _} = Application.ensure_all_started(:paironauts)
-Application.put_env(:wallaby, :base_url, PaironautsWeb.Endpoint.url)
+# Application.put_env(:wallaby, :base_url, PaironautsWeb.Endpoint.url)
